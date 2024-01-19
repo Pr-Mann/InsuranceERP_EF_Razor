1928a95d9be3927612c6b2b700d02a5bc3003acb
Divyang <bansudivyang@gmail.com>
2024-01-19 07:14:24 -0500

    DAL Added

---
+++ InsuranceERP_EF_Razor/DAL/InsuranceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using InsuranceERP_EF_Razor.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace InsuranceERP_EF_Razor.DAL
+{
+    public class InsuranceDbContext : DbContext
+    {
+        public InsuranceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Policy> Policies { get; set; }
+    }
+}
---
+++ InsuranceERP_EF_Razor/Migrations/20240119000151_initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsuranceERP_EF_Razor.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Policies",
+                columns: table => new
+                {
+                    PolicyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PolicyNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HolderName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EffectiveDate = table.Column<DateOnly>(type: "Date", nullable: false),
+                    Premium = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", maxLength: 10000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Policies", x => x.PolicyId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Policies");
+        }
+    }
+}
